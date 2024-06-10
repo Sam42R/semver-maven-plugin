e760a7031a533da984d096c9a1b4c4ba99f0a813
rene <rene@example.com>
2024-06-10 18:19:37 +0200

    :sparkles: (analyzer) add category in addition to type/intention

--- semver-analyzer-api/src/main/java/com/github/sam42r/semver/analyzer/model/AnalyzedCommit.java
+++ semver-analyzer-api/src/main/java/com/github/sam42r/semver/analyzer/model/AnalyzedCommit.java
@@ -9,27 +9,28 @@ import java.util.List;
 import java.util.function.Predicate;
 
 @Data
-@EqualsAndHashCode(callSuper = true)
 @SuperBuilder
+@EqualsAndHashCode(callSuper = false)
 public class AnalyzedCommit extends Commit {
 
     private String header;
     private String body;
     private String footer;
 
-    private Type type;
+    private String type;
+    private Category category;
     private String scope;
     private String subject;
-
     private boolean breaking;
 
     private List<String> issues;
 
-    public enum Type {
+    // TODO reduce enum to categories rendered in changelog
+    public enum Category {
         FIX, FEAT, TEST, BUILD, CI, REFACTOR, DOCS, STYLE, PERF, CHORE;
     }
 
-    public static Predicate<AnalyzedCommit> isBugfix = analyzedCommit -> Type.FIX.equals(analyzedCommit.getType());
-    public static Predicate<AnalyzedCommit> isFeature = analyzedCommit -> Type.FEAT.equals(analyzedCommit.getType());
+    public static Predicate<AnalyzedCommit> isBugfix = analyzedCommit -> Category.FIX.equals(analyzedCommit.getCategory());
+    public static Predicate<AnalyzedCommit> isFeature = analyzedCommit -> Category.FEAT.equals(analyzedCommit.getCategory());
     public static Predicate<AnalyzedCommit> isBreaking = AnalyzedCommit::isBreaking;
 }
