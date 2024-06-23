a0c566f379ea1a631dd97d8d6aec3dc9b93a333f
rene <rene@example.com>
2024-06-23 14:42:55 +0200

    :recycle: rename scm modules #10

---
+++ semver-scm-mecurial/src/main/java/io/github/sam42r/semver/scm/HgProvider.java
@@ -0,0 +1,60 @@
+package io.github.sam42r.semver.scm;
+
+import io.github.sam42r.semver.scm.model.Commit;
+import io.github.sam42r.semver.scm.model.Tag;
+import lombok.NonNull;
+import org.apache.commons.lang3.NotImplementedException;
+
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+/**
+ * {@link SCMProvider} for <a href="https://www.mercurial-scm.org/">mercurial</a>.
+ * TODO implement
+ *
+ * @author Sam42R
+ */
+public class HgProvider implements SCMProviderFactory<HgProvider>, SCMProvider {
+
+    private static final String PROVIDER_NAME = "Mercurial";
+
+    @Override
+    public @NonNull String getProviderName() {
+        return PROVIDER_NAME;
+    }
+
+    @Override
+    public @NonNull HgProvider getInstance(@NonNull Path path) {
+        throw new NotImplementedException();
+    }
+
+    @Override
+    public @NonNull Stream<Commit> readCommits(String fromCommitId) throws SCMException {
+        throw new NotImplementedException();
+    }
+
+    @Override
+    public @NonNull Stream<Tag> readTags() throws SCMException {
+        throw new NotImplementedException();
+    }
+
+    @Override
+    public void addFile(@NonNull Path file) throws SCMException {
+        throw new NotImplementedException();
+    }
+
+    @Override
+    public @NonNull Commit commit(@NonNull String message) throws SCMException {
+        throw new NotImplementedException();
+    }
+
+    @Override
+    public @NonNull Tag tag(@NonNull String name) throws SCMException {
+        throw new NotImplementedException();
+    }
+
+    @Override
+    public void push(boolean force) {
+        throw new NotImplementedException();
+    }
+}
