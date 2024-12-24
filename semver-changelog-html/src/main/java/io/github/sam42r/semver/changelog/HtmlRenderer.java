022740cde296c6167450f34b0bb95e31dcf5a021
rene <rene@example.com>
2024-12-24 17:28:28 +0100

    :sparkles: add changelog HTML renderer #42

--- semver-changelog-html/src/main/java/io/github/sam42r/semver/changelog/HtmlRenderer.java
+++ semver-changelog-html/src/main/java/io/github/sam42r/semver/changelog/HtmlRenderer.java
@@ -4,9 +4,14 @@ import io.github.sam42r.semver.analyzer.model.AnalyzedCommit;
 import io.github.sam42r.semver.changelog.model.VersionInfo;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import org.apache.commons.lang3.NotImplementedException;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
-import java.io.InputStream;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 
@@ -21,6 +26,64 @@ public class HtmlRenderer implements ChangelogRenderer {
             @NonNull VersionInfo versionInfo,
             @NonNull List<AnalyzedCommit> analyzedCommits
     ) {
-        throw new NotImplementedException();
+        var marker = DigestUtils.sha1Hex("Sam42R");
+
+        var alreadyExists = Files.exists(path);
+
+        var templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setPrefix("/templates/%s/".formatted(template));
+        templateResolver.setSuffix(".html");
+
+        var templateEngine = new TemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver);
+        try (
+                var outputStream = new ByteArrayOutputStream();
+                var writer = new OutputStreamWriter(outputStream);
+                var finalOutputStream = new ByteArrayOutputStream();
+                var finalWriter = new BufferedWriter(new OutputStreamWriter(finalOutputStream))
+        ) {
+            var context = new Context();
+
+            context.setVariable("release", versionInfo);
+            context.setVariable("commits", analyzedCommits);
+
+            templateEngine.process(template, context, writer);
+
+            if (alreadyExists) {
+                var markerCount = 0;
+                for (var line : Files.readAllLines(path)) {
+                    finalWriter.write(line);
+                    finalWriter.newLine();
+                    if (line.contains(marker)) {
+                        markerCount++;
+                        if (markerCount == 1) {
+                            writePartial(finalWriter, new ByteArrayInputStream(outputStream.toByteArray()), marker);
+                        }
+                    }
+                }
+            } else {
+                finalWriter.write(outputStream.toString(StandardCharsets.UTF_8));
+            }
+            finalWriter.flush();
+            return new ByteArrayInputStream(finalOutputStream.toByteArray());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void writePartial(Writer writer, InputStream inputStream, String marker) throws IOException {
+        try (var reader = new BufferedReader(new InputStreamReader(inputStream))) {
+            var markerCount = 0;
+            for (var line : reader.lines().toList()) {
+                if (line.contains(marker)) {
+                    markerCount++;
+                } else {
+                    if (markerCount == 1) {
+                        writer.write(line);
+                        writer.append(System.lineSeparator());
+                    }
+                }
+            }
+        }
     }
 }
---
+++ semver-changelog-html/src/test/java/io/github/sam42r/semver/changelog/HtmlRendererTest.java
@@ -0,0 +1,100 @@
+package io.github.sam42r.semver.changelog;
+
+import io.github.sam42r.semver.analyzer.model.AnalyzedCommit;
+import io.github.sam42r.semver.changelog.model.VersionInfo;
+import org.junit.jupiter.api.io.TempDir;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class HtmlRendererTest {
+
+    @TempDir
+    private Path tempDir;
+
+    @ParameterizedTest
+    @CsvSource({"default", "missive", "spinal"})
+    void shouldCreateChangelogFull(String template) throws IOException {
+        var uut = new HtmlRenderer(template);
+        var changelog = tempDir.resolve("Changelog.html");
+
+        try (var inputStream = uut.renderChangelog(changelog, release("v1.0.0"), analyzedCommits())) {
+            var actual = inputStream.readAllBytes();
+            Files.write(changelog, actual);
+
+            assertThat(actual).asString(StandardCharsets.UTF_8)
+                    .startsWith("<!DOCTYPE html>")
+                    .contains("<!-- DO NOT REMOVE - c871f32ed1b7a85b24a0f22e8e7d9e3ee285742c - DO NOT REMOVE -->")
+                    .contains("v1.0.0");
+        }
+    }
+
+    @ParameterizedTest
+    @CsvSource({"default", "missive", "spinal"})
+    void shouldUpdateChangelog(String template) throws IOException {
+        var uut = new HtmlRenderer(template);
+        var changelog = tempDir.resolve("Changelog.html");
+
+        try (var inputStream = uut.renderChangelog(changelog, release("v1.0.0"), analyzedCommits())) {
+            var actual = inputStream.readAllBytes();
+            Files.write(changelog, actual);
+        }
+
+        try (var inputStream = uut.renderChangelog(changelog, release("v1.0.1"), analyzedCommits())) {
+            var actual = inputStream.readAllBytes();
+            Files.write(changelog, actual);
+
+            assertThat(actual).asString(StandardCharsets.UTF_8)
+                    .startsWith("<!DOCTYPE html>")
+                    .contains("<!-- DO NOT REMOVE - c871f32ed1b7a85b24a0f22e8e7d9e3ee285742c - DO NOT REMOVE -->")
+                    .contains("v1.0.1")
+                    .contains("v1.0.0");
+        }
+    }
+
+    private VersionInfo release(String version) {
+        return new VersionInfo(version, LocalDateTime.now().format(DateTimeFormatter.ISO_DATE), "");
+    }
+
+    private List<AnalyzedCommit> analyzedCommits() {
+        return List.of(
+                AnalyzedCommit.builder()
+                        .id("42")
+                        .timestamp(Instant.EPOCH)
+                        .author("JUnit")
+                        .header("feat(scm): Lorem ipsum")
+                        .body("* Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam")
+                        .footer("refs #42")
+                        .category(AnalyzedCommit.Category.ADDED)
+                        .build(),
+                AnalyzedCommit.builder()
+                        .id("42")
+                        .timestamp(Instant.EPOCH)
+                        .author("JUnit")
+                        .header("fix(scm): Lorem ipsum")
+                        .body("* Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam")
+                        .footer("refs #42")
+                        .category(AnalyzedCommit.Category.FIXED)
+                        .build(),
+                AnalyzedCommit.builder()
+                        .id("42")
+                        .timestamp(Instant.EPOCH)
+                        .author("JUnit")
+                        .header("chore(scm): Lorem ipsum")
+                        .body("* Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam")
+                        .footer("refs #42")
+                        .category(AnalyzedCommit.Category.CHANGED)
+                        .build()
+        );
+    }
+}
